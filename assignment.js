447cbd53bab1847a3d2f762014fe3a7e4981b5d9
rjprerona <rownokjahankhanprerona@gmail.com>
2021-08-26 22:11:01 +0600

    more problems added

--- assignment.js
+++ assignment.js
@@ -1,8 +1,8 @@
 
+// https://github.com/rjprerona/js-problem-solving-assignment-2
 
 
-
-// kilometerToMeter
+kilometerToMeter
 
 function kilometerToMeter(meter){
     if(meter < (0)){
@@ -46,8 +46,6 @@ console.log(result);
 
 
 
-// hotelCost
-
 function hotelCost(cost){
     var stay = 0;
     if (cost <= 10){
@@ -71,3 +69,74 @@ function hotelCost(cost){
 
 var result = hotelCost(25);
 console.log(result);
+
+
+
+// seer to mon convert
+
+function seerToMon(mon){
+    if(mon < (0)){
+        return "input cannot be less than zero";
+    }
+    else{
+        return (mon / 40);
+    }
+}
+
+
+// total sales count
+
+ function totalSales(shirt, pant, shoe){
+    var shirtPrice = shirt * 100;
+    var pantPrice = pant * 200;
+    var shoePrice = shoe * 500;
+    var totalPrice = shirtPrice + pantPrice + shoePrice;
+
+        if(totalPrice <= 0){
+            return "input cannot be less than zero";
+        }
+        else{
+            return totalPrice;
+        }
+ }
+   
+
+//  delivery cost count
+
+function deliveryCost(cost){
+    var ship = 0;
+    if (cost <= 100){
+        ship = cost * 100;
+    }
+    else if(cost <= 200){
+        var firstDelivery = 100 * 100;
+        var remaining = cost - 100;
+        var secondDelivery = remaining * 80;
+        ship = firstDelivery + secondDelivery;
+    }
+    else{
+        var firstDelivery = 100 * 100;
+        var secondDelivery = 100 * 80;
+        var remaining = cost - 200;
+        var thirdDelivery = remaining * 50;
+        ship = firstDelivery + secondDelivery + thirdDelivery;
+    }
+    return ship;
+}
+
+
+// perfect friend
+
+function perfectFriend(name){
+        var perfect = name[0];
+        for(var i = 0; i < name.length; i++){
+            var currentName = name[i];
+            if(currentName.length < perfect.length){
+                perfect = currentName;
+            }
+        }
+         return perfect;
+    }
+    var result = perfectFriend(["Moomtahina", "Prerona", "Raisa", "Mahajaben", "Raina"]);
+    console.log(result);
+    
